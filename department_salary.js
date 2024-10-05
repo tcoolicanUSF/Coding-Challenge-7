e3728dca1ac1131b47bffb1963cbcf0b595d4cb5
tcoolican <tcoolican@usf.edu>
2024-10-04 22:21:43 -0400

    Initialize company structure with departments and employees

--- department_salary.js
+++ department_salary.js
@@ -1,1 +1,59 @@
-
+//Coding Challenge 7: Company Department Salary Calculation
+//Task 1: Create a Department Structure
+const company = {
+    departments: [
+      {
+        departmentName: "Racing Department",
+        employees: [
+          {
+            name: "Micheal Schumacher",
+            salary: 500000,
+            subordinates: [
+              {
+                name: "Fernando Alonso",
+                salary: 400000,
+                subordinates: []
+              },
+              {
+                name: "Jenson Buttton",
+                salary: 300000,
+                subordinates: []
+              }
+            ]
+          },
+          {
+            name: "Lewis Hamilton",
+            salary: 600000,
+            subordinates: []
+          }
+        ]
+      },
+      {
+        departmentName: "Engineering Department",
+        employees: [
+          {
+            name: "Max Verstappen",
+            salary: 700000,
+            subordinates: [
+              {
+                name: "Sebastian Vettel",
+                salary: 400000,
+                subordinates: []
+              },
+              {
+                name: "Kamui Kobayashi",
+                salary: 100000,
+                subordinates: []
+              }
+            ]
+          },
+          {
+            name: "Rubens Barrichello",
+            salary: 300000,
+            subordinates: []
+          }
+        ]
+      }
+    ]
+  };
+  
